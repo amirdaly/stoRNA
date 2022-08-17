acde291ba0265123bdcff14cdd3558c8c7629ee9
amirdaly <amirdaly@yahoo.com>
2022-08-17 13:22:47 +0430

    init

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"log"
+	"merkletree/merkletree"
+)
+
+//------------------------------ Testbed Area ----------------------
+
+type TestContent struct {
+	x string
+}
+
+func (t TestContent) CalculateHash() ([]byte, error) {
+	h := sha256.New()
+	if _, err := h.Write([]byte(t.x)); err != nil {
+		return nil, err
+	}
+	return h.Sum(nil), nil
+}
+func (t TestContent) Equals(other merkletree.Content) (bool, error) {
+	return t.x == other.(TestContent).x, nil
+}
+
+func main() {
+	var list []merkletree.Content
+	list = append(list, TestContent{x: "amir"})
+	list = append(list, TestContent{x: "ali"})
+	list = append(list, TestContent{x: "zahra"})
+	list = append(list, TestContent{x: "sara"})
+
+	t, err := merkletree.NewTree(list)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(t)
+
+}
